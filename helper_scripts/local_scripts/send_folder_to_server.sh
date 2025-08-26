d738280e4803d9a4bccfe5050bf32824d7c133b0
Soney <sonephyo7777777@gmail.com>
2025-08-26 13:48:30 -0400

    config: changing the bitezone location

--- helper_scripts/local_scripts/send_folder_to_server.sh
+++ helper_scripts/local_scripts/send_folder_to_server.sh
@@ -1,1 +1,1 @@
-rsync -av --exclude='.venv' --exclude='./compiledFiles' --exclude='.git' --exclude='.gitignore' --exclude='.DS_Store' --exclude='playground' ./ pphyo@cs.oswego.edu:/home/pphyo/CSC495/web-scraping
+rsync -av --exclude='.venv' --exclude='./compiledFiles' --exclude='.git' --exclude='.gitignore' --exclude='.DS_Store' --exclude='playground' ./ pphyo@cs.oswego.edu:/home/pphyo/bitezone/web-scraping
